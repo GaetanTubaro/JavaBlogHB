d0aff38b00e5cec7fe2c4d06204225741a4c1f39
gat <tubaro.gaetan@gmail.com>
2023-02-10 17:12:03 +0100

    oublie modelAttribute

--- src/main/java/com/hb/blog/controllers/CommentController.java
+++ src/main/java/com/hb/blog/controllers/CommentController.java
@@ -3,10 +3,7 @@ package com.hb.blog.controllers;
 import com.hb.blog.dtos.CommentDTO;
 import com.hb.blog.services.CommentService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
@@ -40,7 +37,7 @@ public class CommentController {
         return mav;
     }
     @PostMapping("/new")
-    public ModelAndView addNewComment(CommentDTO commentDTO) {
+    public ModelAndView addNewComment(@ModelAttribute CommentDTO commentDTO) {
         commentService.addNewComment(commentDTO);
         return new ModelAndView("redirect:/private/comment/" + commentDTO.postId());
     }
